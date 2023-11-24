ac85f3a0617c545cec48589d14e2a481b8d7eab0
Ardian Osmani <realardosmani@gmail.com>
2023-11-24 13:08:39 +0100

    changes

--- components/Context.js
+++ components/Context.js
@@ -1,1 +1,10 @@
 import React,{createContext,useState} from "react";
+
+export const item=createContext()
+
+export const Context =({children})=>{
+    const [myBag,set]= useState(initialState);
+    return <item.Provider value={}>
+        {children}
+    </item.Provider>
+}
